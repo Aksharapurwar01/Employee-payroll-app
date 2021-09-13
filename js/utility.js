591cfecc8e41f322a30eebb6bd4060ba89dcea68
Akshara Purwar <aksharapurwar@gmail.com>
2021-09-13 14:47:06 +0530

    Employee payroll app with home page,payroll-form

---
+++ js/utility.js
@@ -0,0 +1,8 @@
+const stringifyDate = (date) => {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    return !date ? 'undefined' : new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
+}
+
+const update = (node) =>{
+    
+}
